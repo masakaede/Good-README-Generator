4b0d46df6e746e204f826ae5810c7521508aae31
masakaede <pichengchang@gmail.com>
2020-10-16 23:53:40 +1100

    move questions to separate js file

--- README2.md
+++ README2.md
@@ -1,4 +1,4 @@
-# Project A
+# Good-README-Generator
 
 ## Description
 A command-line application that dynamically generates a professional README.md from a user’s input
@@ -14,46 +14,41 @@ A command-line application that dynamically generates a professional README.md f
 ## Installation
 1.	Make sure node.js is installed
 2.	You can type the following code at terminal to check if node.js is installed
->  node –version
-
+	>  node –version
 3.	If node version doesn’t show up, you can download it from https://nodejs.org/en/download/
 4.	The files you need to run the program are index.js and package.json
 5.	Type the following code to install package
-```
-npm install
-```
+	>  npm install
 
 
 ## Usage
 1.	Type the following code at terminal to run the CLI
-```
-node index.js
-```
+	>  node index.js
 2.	The following questions will be prompted to be answered
--	Project name
--	Project description
--	Installation instructions
--	Usage information
--	Contributing
--	Tests
--	Licence
--	Github username
--	Email address
+	-	Project name
+	-	Project description
+	-	Installation instructions
+	-	Usage information
+	-	Contributing
+	-	Tests
+	-	Licence
+	-	Github username
+	-	Email address
 3.	Once all the questions answered, README.md will be created
 
 
 ## License
-[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
 
 ## Contributing
 Please submit a **Pull request** if you would like to contribute to the project
 
 ## Tests
-tests
+Tests
 
 ## Questions
-* Follow me at: <a href="https://github.com/masa" target="_blank">https://github.com/masa</a>
+* Follow me at: <a href="https://github.com/masakaede" target="_blank">https://github.com/masakaede</a>
 
 * Please email to gmail if you have any question
 
-© 2020 masa
+© 2020 masakaede
--- index.js
+++ index.js
@@ -1,79 +1,75 @@
 const fs = require("fs");
 const util = require("util");
-// const userInput = require("./assets/js/user-input")
-const inquirer = require("inquirer");
+const userInput = require("./assets/js/user-input")
+// const inquirer = require("inquirer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What's the project name?"
-        },
-        {
-            type: "input",
-            name: "description",
-            message: "What's the description of the project?"
-        },
-        {
-            type: "editor",
-            name: "instruction",
-            message: "Please enter installation instructions?"
-        },
-        {
-            type: "editor",
-            name: "usage",
-            message: "Please enter usage information?"
-        },
-        {
-            type: "list",
-            name: "licence",
-            message: "Please choose your licence (use arrow key to select)",
-            choices: [
-                "Apache",
-                "MIT",
-                "BSD",
-                "GPL",
-                "Mozilla",
-                "LGPL"
-            ]
-        },
-        {
-            type: "input",
-            name: "contributing",
-            message: "Please enter contributing"
-        },
-        {
-            type: "input",
-            name: "tests",
-            message: "Please enter tests"
-        },
-        {
-            type: "input",
-            name: "username",
-            message: "Please enter your github username"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Please enter your email address"
-        }
-
-    ])
-}
-
-
-
+// function promptUser() {
+//     return inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "name",
+//             message: "What's the project name?"
+//         },
+//         {
+//             type: "input",
+//             name: "description",
+//             message: "What's the description of the project?"
+//         },
+//         {
+//             type: "editor",
+//             name: "instruction",
+//             message: "Please enter installation instructions?"
+//         },
+//         {
+//             type: "editor",
+//             name: "usage",
+//             message: "Please enter usage information?"
+//         },
+//         {
+//             type: "list",
+//             name: "licence",
+//             message: "Please choose your licence (use arrow key to select)",
+//             choices: [
+//                 "Apache",
+//                 "MIT",
+//                 "BSD",
+//                 "GPL",
+//                 "Mozilla",
+//                 "LGPL"
+//             ]
+//         },
+//         {
+//             type: "input",
+//             name: "contributing",
+//             message: "Please enter contributing"
+//         },
+//         {
+//             type: "input",
+//             name: "tests",
+//             message: "Please enter tests"
+//         },
+//         {
+//             type: "input",
+//             name: "username",
+//             message: "Please enter your github username"
+//         },
+//         {
+//             type: "input",
+//             name: "email",
+//             message: "Please enter your email address"
+//         }
+//     ])
+// }
 
 var answers = {}
 var licence = ""
 async function userData() {
     try {
-        answers = await promptUser();
+        answers = await userInput.promptUser();
 
-        console.log("51" + answers.name)
+        console.log("72" + answers.name)
 
 
         switch (answers.licence) {
@@ -97,7 +93,7 @@ async function userData() {
                 break;
         }
 
-        console.log("59 Successfully read answer");
+        console.log("96 Successfully read answer");
     } catch (err) {
         console.log(err)
     }
@@ -113,8 +109,8 @@ async function userData() {
 //     }
 // }
 userData().then(function () {
-    console.log("76" + answers.description)
-    console.log("77" + answers)
+    console.log("112" + answers.description)
+    console.log("113" + answers)
 
     var date = new Date();
     var year = date.getFullYear();
@@ -159,10 +155,10 @@ ${answers.tests}
         if (err) {
             return console.log(err)
         }
-        console.log("87 Successfully wrote to README.md");
+        console.log("158 Successfully wrote to README.md");
     }
     );
-    console.log("90" + JSON.stringify(answers))
+    console.log("161" + JSON.stringify(answers))
 
 })
 // createFile()
